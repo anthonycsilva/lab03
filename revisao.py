1324adf906387fefc34af9034835291dc09afe0f
Anthony Coutinho <anthonycoutinho@poli.ufrj.br>
2020-12-13 20:52:05 -0300

    Atualizando o README e adicionando a descrição de Strings

--- revisao.py
+++ revisao.py
@@ -10,3 +10,12 @@ c  = 3j
 k = random.randint(1,10) + ((random.randint(1,10) + 3j))
 
 print('Numeros Complexos, onde J é imaginário: {}, {}'.format(c, k))
+
+s = 'Isso é uma string, String é tudo escrito dentro das aspas simples ou duplas!'
+exemplo1 = 'Como isso é uma string o python vê isso como: 3 + 5'
+print(s)
+print(exemplo1)
+
+conta = '3+5'
+
+print('A  conta só retorna o texto escrito!: {}'.format(conta))
