5cb622b800ec5642d5e62e7ad76c44ecc027efd1
Anthony Coutinho <anthonycoutinho@poli.ufrj.br>
2020-12-13 20:42:25 -0300

    Adicionando  Numeros Complexos

--- revisao.py
+++ revisao.py
@@ -5,3 +5,8 @@ print('Inteiros: {}'.format(x))
 
 f = (random.random())
 print('Float entre 0 e 1:  {:.4}'.format(f))
+
+c  = 3j
+k = random.randint(1,10) + ((random.randint(1,10) + 3j))
+
+print('Numeros Complexos, onde J é imaginário: {}, {}'.format(c, k))
