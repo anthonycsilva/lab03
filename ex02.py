71c9a3255e329c7faf37aa7e6ff0ea20e5eda108
Anthony Coutinho <anthonycoutinho@poli.ufrj.br>
2020-12-15 22:04:43 -0300

    Ex01,Ex02,Ex03,Ex04 Adicionados :)

---
+++ ex02.py
@@ -0,0 +1,32 @@
+import math
+def discriminante(a,b,c):
+#essa função calcula o discriminante dado as variáveis a,b,c
+#int, int, int -> int
+   return math.pow(b, 2) - (4*a*c)
+
+
+
+def calculaRaiz1(a,b,c):
+#essa função calcula a primeira raiz da equação  de segundo grau
+#int, int, int -> float
+
+    return (-(b)  + math.sqrt(discriminante(a,b,c)))/2*a
+
+
+
+def calculaRaiz2(a,b,c):
+#essa função calcula a segunda raiz da equação  de segundo grau
+#int, int, int -> float
+    return (-(b) - math.sqrt(discriminante(a,b,c)))/2*a
+
+def verificaRaiz(a,b,c):
+    if(discriminante(a,b,c)<0):
+        return 0
+    elif(discriminante(a,b,c) == 0):
+        return  calculaRaiz1(a,b,c)
+    elif(discriminante(a,b,c) > 0):
+        return calculaRaiz1(a,b,c), calculaRaiz2(a,b,c)
+
+print(verificaRaiz(1,-3, -10))
+
+
---
+++ ex04.py
@@ -0,0 +1,6 @@
+def data(d,m,a):
+#essa função retorna em os numeros inseridos como dia/mes/ano
+
+    return str(d)+'/'+str(m)+'/'+str(a)
+
+
