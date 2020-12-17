f42d45ab50f0cdbadb21b1f32a1e3dde04880797
anthonycsilva <anthonycoutinho@poli.ufrj.br>
2020-12-17 18:02:56 -0300

    Atualização conforme o escopo, dia, mes e data.

--- ex04.py
+++ ex04.py
@@ -1,7 +1,13 @@
 def data(d,m,a):
     '''Essa função retorna em os numeros inseridos como dia/mes/ano'''
     '''int -> int'''
-
-    return str(d)+'/'+str(m)+'/'+str(a)
+    if (d < 10 and m < 10):
+        return str(0)+str(d)+'/'+str(0)+str(m)+'/'+str(a)
+    elif(m < 10):
+        return str(d)+'/'+str(0)+str(m)+'/'+str(a)
+    elif(d < 10):
+        return str(0) + str(d) + '/' + str(m) + '/' + str(a)
+    else:
+        return str(d)+'/'+str(m)+'/'+str(a)
 
 
